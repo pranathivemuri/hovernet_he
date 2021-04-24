59823f747ca52a533f9cbf5421cdb89dd8163e02
pranathi.vemuri <pranathi93.vemuri@gmail.com>
2021-04-24 02:06:08 +0000

    wip

--- run_wsi.sh
+++ run_wsi.sh
@@ -1,10 +1,10 @@
 python run_infer.py \
 --gpu='0,1' \
---nr_types=6 \
+--nr_types=5 \
 --type_info_path=type_info.json \
---batch_size=64 \
+--batch_size=1 \
 --model_mode=fast \
---model_path=../pretrained/hovernet_fast_pannuke_type_tf2pytorch.tar \
+--model_path="/data/hovernet_training_data/tabsap/logs/01/net_epoch=50.tar" \
 --nr_inference_workers=8 \
 --nr_post_proc_workers=16 \
 wsi \
